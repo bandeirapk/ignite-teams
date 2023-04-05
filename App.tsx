522d3bb78bde13de1d304bbd4d21078e6555d89f
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2023-04-05 10:31:22 -0300

    :wrench: chore: adicionado o pacote react-navigation

--- App.tsx
+++ App.tsx
@@ -2,7 +2,11 @@ import { StatusBar } from 'react-native';
 
 import { ThemeProvider } from 'styled-components';
 
-import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
 
@@ -15,17 +19,17 @@ import { Loading } from '@components/Loading';
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
-  })
+    Roboto_700Bold,
+  });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <Players /> : <Loading /> }  
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
